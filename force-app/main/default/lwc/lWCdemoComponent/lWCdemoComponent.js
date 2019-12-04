43183b897defa2cdd30a08c4a7cccbb2cdb8b1ac
unknown <ashish.sharma@manras.com>
2019-12-04 18:52:44 +0530

    create contact LWC

---
+++ force-app/main/default/lwc/lWCdemoComponent/lWCdemoComponent.js
@@ -0,0 +1,15 @@
+import { LightningElement, track, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+    export default class LWCdemoComponent extends LightningElement {
+        @api recordId;
+        @track cardTitle='New Contact';
+        handleSuccess(){
+            const evt = new ShowToastEvent({
+                title: "Success!",
+                message: "The Contact's record has been successfully saved.",
+                variant: "success",
+            });
+            this.dispatchEvent(evt);
+         
+        }
+    }
